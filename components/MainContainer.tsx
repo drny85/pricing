1227d0571c3b5eeb2f3bb1ce4877da2140ad4f7c
Robert Melendez <37252834+drny85@users.noreply.github.com>
2023-03-16 11:58:00 -0400

    update MainComponent

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -3,9 +3,11 @@ import React, { FC } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hooks/reduxHooks';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { switchTheme } from '../redux/themeSlide';
 import { darkTheme, lightTheme } from '../Theme';
 import Head from 'next/head';
+import { auth } from '../firebase';
 
 interface PageProps {
     children: React.ReactNode;
@@ -38,33 +40,53 @@ const MainContainer: FC<PageProps> = ({ children, title }) => {
             >
                 <Head>{title}</Head>
                 {children}
-                <Fab
-                    onClick={() =>
-                        dispatch(
-                            switchTheme(
-                                theme.mode === 'dark' ? lightTheme : darkTheme
-                            )
-                        )
-                    }
-                    style={{
-                        position: 'fixed',
-                        left: '30px',
-                        bottom: '30px',
-                        zIndex: 9999,
-                        backgroundColor: theme.TEXT_COLOR,
-                    }}
+                <footer
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
                 >
-                    {theme.mode === 'light' ? (
-                        <DarkModeIcon
-                            color="secondary"
-                            style={{ backgroundColor: theme.TEXT_COLOR }}
-                        />
-                    ) : (
-                        <LightModeIcon
-                            style={{ backgroundColor: theme.TEXT_COLOR }}
-                        />
-                    )}
-                </Fab>
+                    <Fab
+                        onClick={() =>
+                            dispatch(
+                                switchTheme(
+                                    theme.mode === 'dark'
+                                        ? lightTheme
+                                        : darkTheme
+                                )
+                            )
+                        }
+                        style={{
+                            position: 'fixed',
+                            left: '30px',
+                            bottom: '30px',
+                            zIndex: 9999,
+                            backgroundColor: theme.TEXT_COLOR,
+                        }}
+                    >
+                        {theme.mode === 'light' ? (
+                            <DarkModeIcon
+                                color="secondary"
+                                style={{ backgroundColor: theme.TEXT_COLOR }}
+                            />
+                        ) : (
+                            <LightModeIcon
+                                style={{ backgroundColor: theme.TEXT_COLOR }}
+                            />
+                        )}
+                    </Fab>
+                    <Fab
+                        onClick={() => {
+                            auth.signOut();
+                        }}
+                        style={{
+                            position: 'fixed',
+                            right: '30px',
+                            bottom: '30px',
+                            zIndex: 9999,
+                            backgroundColor: theme.TEXT_COLOR,
+                        }}
+                    >
+                        <ExitToAppIcon color="primary" />
+                    </Fab>
+                </footer>
             </div>
         </div>
     );
