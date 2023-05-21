b5c8dcbe23b4fb29c083980ea3235acc608e7c7a
Robert Melendez <37252834+drny85@users.noreply.github.com>
2023-05-21 14:45:14 -0400

    Add files via upload

---
+++ perks.ts
@@ -0,0 +1,93 @@
+import { Perk } from './components/PerksView';
+
+export const perks: Perk[] = [
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-disney-bundle-1x1-small?fmt=webp-alpha&scl=2',
+        name: 'disney bundle',
+        price: 10,
+        sharabled: true,
+        value: 14.99,
+        selected: false,
+        description:
+            'The Disney Bundle is the “Something for Everyone” streaming solution from The Walt Disney Company. With the Disney Bundle through Verizon, you get access to thousands of movies, hit series and exclusive sports with subscriptions to Disney+ (No Ads), Hulu (With Ads) and ESPN+ (With Ads).',
+    },
+    {
+        image: 'https://www.apple.com/v/apple-one/d/images/meta/og__diu4z5hyr8ia_overview.png',
+        name: 'apple one',
+        price: 10,
+        sharabled: false,
+        value: 16.99,
+        selected: false,
+        description:
+            'Apple One bundles together Apple Music®, Apple TV+®, Apple Arcade®, and iCloud+℠ with up to 200 GB of storage. Plus, it’s all ad-free. \n Apple One is available as an Individual or Family subscription through Verizon.',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-mobile-hotspot-1x1-small?fmt=webp-alpha&scl=2',
+        name: '100 GB mobile hotspot',
+        price: 10,
+        value: 45,
+        selected: false,
+        sharabled: false,
+        description:
+            'A Mobile Hotspot lets you share your Verizon network connection with other devices so they can access the internet. Devices connect to your Mobile Hotspot using Wi-Fi. While devices are connected, you’re charged for any data they use according to your monthly data plan.',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/walmart_pop_plans_d?fmt=webp-alpha&scl=2',
+        name: 'walmart+ membership',
+        price: 10,
+        selected: false,
+        sharabled: true,
+        value: 12.95,
+        description:
+            'Walmart+ is a membership from Walmart that gives you access to lots of benefits that will help you save time and money.',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-apple-music-family-1x1-small?fmt=webp-alpha&scl=2',
+        name: 'apple music family',
+        price: 10,
+        selected: false,
+        value: 16.99,
+        sharabled: true,
+        description: 'this a description for the perk',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-smartwatch-data-safety-3x2-small?scl=2.5&fmt=webp',
+        name: 'smartwatch data & safety',
+        price: 10,
+        value: 20,
+        selected: false,
+        sharabled: false,
+        description:
+            'Connected device plans are monthly data plans for your non-phone devices (i.e., mobile hotspot devices, USB modems, tablets, laptops, smartwatches, security cameras, in-car Wi-Fi, and more).',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-plus-play-monthly-credit-3x2-small?scl=2.5&fmt=webp',
+        name: '+play monthly credit',
+        price: 10,
+        selected: false,
+        value: 15,
+        sharabled: false,
+        description:
+            'Get $15 to spend each month on your favorite +play subscriptions for just $10. Get Netflix, Xbox Game Pass, HBO Max, Paramount+, MasterClass, and more - for less!',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-two-tb-cloud-storage-3x2-small?scl=2.5&fmt=webp',
+        name: '2 TB cloud storage',
+        price: 10,
+        selected: false,
+        value: 14.99,
+        sharabled: true,
+        description:
+            'Get $15 to spend each month on your favorite +play subscriptions for just $10. Get Netflix, Xbox Game Pass, HBO Max, Paramount+, MasterClass, and more - for less!',
+    },
+    {
+        image: 'https://ss7.vzw.com/is/image/VerizonWireless/perk-tile-three-travelpass-days-3x2-small?scl=2.5&fmt=webp',
+        name: '3 TravelPass Days',
+        price: 10,
+        selected: false,
+        sharabled: false,
+        value: 30,
+        description:
+            'TravelPass lets you use your domestic talk, text and data* in 210+ countries and destinations outside the US. You can call within the country you’re visiting and you can call back to the US.',
+    },
+];
