20d4ddbdc9a64b1b2a10fa880b7a680cc3068ad0
Robert Melendez <37252834+drny85@users.noreply.github.com>
2023-05-21 20:38:24 -0400

    Add files via upload

--- utils/byodSavings.ts
+++ utils/byodSavings.ts
@@ -1,12 +1,13 @@
+import { NON_PREMIUM_BYOD_VALUE, PREMIUM_BYOD_VALUE } from '../constant';
 import { Line } from '../redux/wirelessSlide';
 
 export const byodSavings = (lines: Line[]): number =>
     lines
         .map((line) =>
             line.byod && line.name === 'Unlimited Plus'
-                ? { discount: 540 / 36 }
+                ? { discount: PREMIUM_BYOD_VALUE }
                 : line.byod && line.name === 'Unlimited Welcome'
-                ? { discount: 180 / 36 }
+                ? { discount: NON_PREMIUM_BYOD_VALUE }
                 : { discount: 0 }
         )
         .reduce((acc, line) => acc + line.discount, 0);
